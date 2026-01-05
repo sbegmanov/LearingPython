23de0d38225d2f91d35b8f4b77e47c29b0590306
sbegmanov <sbegmanov@gmail.com>
2026-01-05 17:54:56 +0500

    Chapter 10

--- code/practice.py
+++ code/practice.py
@@ -6,3 +6,68 @@ while True:
 #  with := assignment expression 
 while (reply := input('Enter text: ')) != 'stop':
     print(reply.upper())
+
+# Doing a math
+while True:
+    reply = input('Enter text integer:')
+    if reply == 'stop': break
+    print(int(reply) ** 2)
+print('Bye')
+
+# Handling error
+while True:
+    reply = input('Enter text: ')
+    if reply == 'stop':
+        break
+    elif not reply.isdigit():
+        print('Bad!' * 8)
+    else:
+        print(int(reply) ** 2)
+print('Bye')
+
+# Handling Errors with try Statements
+while True:
+    reply = input('Enter text:')
+    if reply == 'stop': break
+    try:
+        num = int(reply)
+    except:
+        print('Bad!' * 8)
+    else:
+        print(num ** 2)
+print('Bye')
+
+#  If we’re sure that print won’t fail
+while True:
+    reply = input('Enter text:')
+    if reply == 'stop': break
+    try:
+        print(int(reply) ** 2)
+    except:
+        print('Bad!' * 8)
+print('Bye')
+
+# Supporting Floating-Point Numbers
+while True:
+    reply = input('Enter text:')
+    if reply == 'stop': break
+    try:
+        print(float(reply) ** 2)
+    except:
+        print('Bad!' * 8)
+print('Bye')
+
+# Nesting Code Three Levels Deep
+while True:
+    reply = input('Enter text:')
+    if reply == 'stop':
+        break
+    elif not reply.isdigit():
+        print('Bad!' * 8)
+    else:
+        num = int(reply)
+        if num < 20:
+            print('low')
+        else:
+            print(num ** 2)
+print('Bye')
